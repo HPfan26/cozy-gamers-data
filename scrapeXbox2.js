c968d88c1bd9710144669e8fc7c443a27a6ccac0
HPfan26 <77130222+HPfan26@users.noreply.github.com>
2026-05-13 19:58:41 +0100

    Update scrapeXbox2.js

--- scrapeXbox2.js
+++ scrapeXbox2.js
@@ -7,363 +7,94 @@ puppeteer.use(StealthPlugin());
 const gameList = [
   {
     title: 'Stardew Valley',
-    slug: {
-      default: 'stardew-valley',
-      'th-th': 'stardew-valley',
-      'en-ph': 'stardew-valley'
-    },
-    productId: {
-      default: 'C3D891Z6TNQM',
-      'th-th': '9mwr1nc6vq6l',
-      'en-ph': '9MWR1NC6VQ6L/0010'
-    }
+    slug: { default: 'stardew-valley', 'th-th': 'stardew-valley', 'en-ph': 'stardew-valley' },
+    productId: { default: 'C3D891Z6TNQM', 'th-th': '9mwr1nc6vq6l', 'en-ph': '9MWR1NC6VQ6L/0010' }
   },
-  {
-    title: 'Fae Farm',
-    slug: 'fae-farm',
-    productId: '9N0QS2277QHF'
-  },
+  { title: 'Fae Farm', slug: 'fae-farm', productId: '9N0QS2277QHF' },
   {
     title: 'Dredge',
-    slug: {
-      default: 'dredge',
-      'pl-pl': 'dredge'
-    },
-    productId: {
-      default: '9msvvm5ns9l6',
-      'pl-pl': '9MSVVM5NS9L6'
-    }
+    slug: { default: 'dredge', 'pl-pl': 'dredge' },
+    productId: { default: '9msvvm5ns9l6', 'pl-pl': '9MSVVM5NS9L6' }
   },
-  {
-    title: 'Wanderstop',
-    slug: 'wanderstop',
-    productId: '9p2195r38k3j'
-  },
-  {
-    title: 'A Little to the Left',
-    slug: 'a-little-to-the-left',
-    productId: '9nlddf97gtj0'
-  },
-  {
-    title: 'Toem',
-    slug: 'toem',
-    productId: '9n7zcllr01b3'
-  },
-  {
-    title: 'Paper Trail',
-    slug: 'paper-trail',
-    productId: '9p4xm3tj13qm'
-  },
-  {
-    title: 'Little Kitty, Big City',
-    slug: 'little-kitty-big-city',
-    productId: '9nf5s7mlm8xt'
-  },
-  {
-    title: 'Lonesome Village',
-    slug: 'lonesome-village',
-    productId: '9P8ND0L4CXPR'
-  },
-  {
-    title: 'Lake',
-    slug: 'lake',
-    productId: '9n5glftt40sn'
-  },
+  { title: 'Wanderstop', slug: 'wanderstop', productId: '9p2195r38k3j' },
+  { title: 'A Little to the Left', slug: 'a-little-to-the-left', productId: '9nlddf97gtj0' },
+  { title: 'Toem', slug: 'toem', productId: '9n7zcllr01b3' },
+  { title: 'Paper Trail', slug: 'paper-trail', productId: '9p4xm3tj13qm' },
+  { title: 'Little Kitty, Big City', slug: 'little-kitty-big-city', productId: '9nf5s7mlm8xt' },
+  { title: 'Lonesome Village', slug: 'lonesome-village', productId: '9P8ND0L4CXPR' },
+  { title: 'Lake', slug: 'lake', productId: '9n5glftt40sn' },
   {
     title: 'Cozy Grove',
-    slug: {
-      default: 'cozy-grove',
-      'th-th': 'cozy-grove-new-neighbears-bundle',
-      'en-ph': 'cozy-grove-new-neighbears-bundle'
-    },
-    productId: {
-      default: '9pjcc91cz3wn',
-      'th-th': '9P481LTRXR7N',
-      'en-ph': '9P481LTRXR7N'
-    }
+    slug: { default: 'cozy-grove', 'th-th': 'cozy-grove-new-neighbears-bundle', 'en-ph': 'cozy-grove-new-neighbears-bundle' },
+    productId: { default: '9pjcc91cz3wn', 'th-th': '9P481LTRXR7N', 'en-ph': '9P481LTRXR7N' }
   },
-  {
-    title: 'Lil Gator Game',
-    slug: 'lil-gator-game',
-    productId: '9n4fhwk9hwdk'
-  },
-  {
-    title: 'A Short Hike',
-    slug: 'a-short-hike',
-    productId: '9nk78df207sd'
-  },
+  { title: 'Lil Gator Game', slug: 'lil-gator-game', productId: '9n4fhwk9hwdk' },
+  { title: 'A Short Hike', slug: 'a-short-hike', productId: '9nk78df207sd' },
   {
     title: 'The Sims 4',
-    slug: {
-      default: 'the-sims-4',
-      'th-th': 'the-sims-4-ea-play-edition',
-      'en-ph': 'the-sims-4-ea-play-edition'
-    },
-    productId: {
-      default: 'C08JXNK0VG5L',
-      'th-th': '9PB5T776N2SW',
-      'en-ph': '9PB5T776N2SW'
-    }
+    slug: { default: 'the-sims-4', 'th-th': 'the-sims-4-ea-play-edition', 'en-ph': 'the-sims-4-ea-play-edition' },
+    productId: { default: 'C08JXNK0VG5L', 'th-th': '9PB5T776N2SW', 'en-ph': '9PB5T776N2SW' }
   },
   {
     title: 'My Time at Portia',
-    slug: {
-      default: 'my-time-at-portia',
-      'th-th': 'my-time-at-portia',
-      'en-ph': 'my-time-at-portia'
-    },
-    productId: {
-      default: 'bx1fzx1x4132',
-      'th-th': '9nblsn1jp7th',
-      'en-ph': '9nblsn1jp7th'
-    }
+    slug: { default: 'my-time-at-portia', 'th-th': 'my-time-at-portia', 'en-ph': 'my-time-at-portia' },
+    productId: { default: 'bx1fzx1x4132', 'th-th': '9nblsn1jp7th', 'en-ph': '9nblsn1jp7th' }
   },
-  {
-    title: 'My Time at Sandrock',
-    slug: 'my-time-at-sandrock',
-    productId: '9nhs5p1sjhn6'
-  },
+  { title: 'My Time at Sandrock', slug: 'my-time-at-sandrock', productId: '9nhs5p1sjhn6' },
   {
     title: 'Ooblets',
-    slug: {
-      default: 'ooblets',
-      'th-th': 'ooblets',
-      'en-ph': 'ooblets'
-    },
-    productId: {
-      default: '9NB4GBLPTD0S',
-      'th-th': '9NB4GBLPTD0S',
-      'en-ph': '9NB4GBLPTD0S'
-    },
+    slug: { default: 'ooblets', 'th-th': 'ooblets', 'en-ph': 'ooblets' },
+    productId: { default: '9NB4GBLPTD0S', 'th-th': '9NB4GBLPTD0S', 'en-ph': '9NB4GBLPTD0S' },
     unavailable: ['de-de', 'fr-fr', 'pt-pt', 'es-es', 'sv-se', 'nl-nl', 'pl-pl', 'da-dk', 'cs-cz', 'hu-hu', 'fi-fi']
   },
-  {
-    title: 'Firewatch',
-    slug: 'firewatch',
-    productId: 'bqqkg9h2stc0'
-  },
-  {
-    title: 'Cities: Skylines',
-    slug: 'cities-skylines-remastered',
-    productId: '9mz4gbwx9gnd'
-  },
-  {
-    title: 'Unpacking',
-    slug: 'unpacking',
-    productId: '9nh5hn11fg4m'
-  },
-  {
-    title: 'Strange Horticulture',
-    slug: 'strange-horticulture',
-    productId: '9mx1b2l6zm36',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Disney Dreamlight Valley',
-    slug: 'disney-dreamlight-valley',
-    productId: '9nsf0bgh8d86'
-  },
-  {
-    title: 'Coffee Talk',
-    slug: 'coffee-talk',
-    productId: '9n17cm38wnn8'
-  },
-  {
-    title: 'Spirit of the North',
-    slug: 'spirit-of-the-north-enhanced-edition',
-    productId: '9mvl42njfvz2'
-  },
-  {
-    title: 'Neva',
-    slug: 'neva',
-    productId: '9n2ln3wrt82h'
-  },
-  {
-    title: 'Wytchwood',
-    slug: 'wytchwood',
-    productId: '9p40fjdr9bzr'
-  },
-  {
-    title: 'Lemon Cake',
-    slug: 'lemon-cake',
-    productId: '9nv9dkbpc663'
-  },
-  {
-    title: 'Paleo Pines',
-    slug: 'paleo-pines',
-    productId: '9nq5z2wh4xdb',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Cult of the Lamb',
-    slug: 'cult-of-the-lamb',
-    productId: '9pnlpmp1ggh5'
-  },
-  {
-    title: 'Roots of Pacha',
-    slug: 'roots-of-pacha',
-    productId: '9nbzz380pd19',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Coral Island',
-    slug: 'coral-island',
-    productId: '9n1t1mq92pvs'
-  },
-  {
-    title: 'Potion Permit',
-    slug: 'potion-permit',
-    productId: '9njlqw4xl3mw',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Potion Craft',
-    slug: 'potion-craft-alchemist-simulator',
-    productId: '9mw7wd7j3ppk'
-  },
-  {
-    title: 'Slime Rancher',
-    slug: 'slime-rancher',
-    productId: 'c2nc88m7nwz1'
-  },
-  {
-    title: 'Slime Rancher 2',
-    slug: 'slime-rancher-2',
-    productId: '9phfj0n31nv1'
-  },
+  { title: 'Firewatch', slug: 'firewatch', productId: 'bqqkg9h2stc0' },
+  { title: 'Cities: Skylines', slug: 'cities-skylines-remastered', productId: '9mz4gbwx9gnd' },
+  { title: 'Unpacking', slug: 'unpacking', productId: '9nh5hn11fg4m' },
+  { title: 'Strange Horticulture', slug: 'strange-horticulture', productId: '9mx1b2l6zm36', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Disney Dreamlight Valley', slug: 'disney-dreamlight-valley', productId: '9nsf0bgh8d86' },
+  { title: 'Coffee Talk', slug: 'coffee-talk', productId: '9n17cm38wnn8' },
+  { title: 'Spirit of the North', slug: 'spirit-of-the-north-enhanced-edition', productId: '9mvl42njfvz2' },
+  { title: 'Neva', slug: 'neva', productId: '9n2ln3wrt82h' },
+  { title: 'Wytchwood', slug: 'wytchwood', productId: '9p40fjdr9bzr' },
+  { title: 'Lemon Cake', slug: 'lemon-cake', productId: '9nv9dkbpc663' },
+  { title: 'Paleo Pines', slug: 'paleo-pines', productId: '9nq5z2wh4xdb', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Cult of the Lamb', slug: 'cult-of-the-lamb', productId: '9pnlpmp1ggh5' },
+  { title: 'Roots of Pacha', slug: 'roots-of-pacha', productId: '9nbzz380pd19', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Coral Island', slug: 'coral-island', productId: '9n1t1mq92pvs' },
+  { title: 'Potion Permit', slug: 'potion-permit', productId: '9njlqw4xl3mw', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Potion Craft', slug: 'potion-craft-alchemist-simulator', productId: '9mw7wd7j3ppk' },
+  { title: 'Slime Rancher', slug: 'slime-rancher', productId: 'c2nc88m7nwz1' },
+  { title: 'Slime Rancher 2', slug: 'slime-rancher-2', productId: '9phfj0n31nv1' },
   {
     title: 'Graveyard Keeper',
-    slug: {
-      default: 'graveyard-keeper',
-      'th-th': 'graveyard-keeper-last-journey-edition',
-      'en-in': 'graveyard-keeper-last-journey-edition',
-      'en-ph': 'graveyard-keeper-last-journey-edition'
-    },
-    productId: {
-      default: 'c11gzgmkrtcv',
-      'en-in': 'c11gzgmkrtcv',
-      'th-th': '9PPJV0ZDBSKT',
-      'en-ph': '9PPJV0ZDBSKT'
-    }
+    slug: { default: 'graveyard-keeper', 'th-th': 'graveyard-keeper-last-journey-edition', 'en-in': 'graveyard-keeper-last-journey-edition', 'en-ph': 'graveyard-keeper-last-journey-edition' },
+    productId: { default: 'c11gzgmkrtcv', 'en-in': 'c11gzgmkrtcv', 'th-th': '9PPJV0ZDBSKT', 'en-ph': '9PPJV0ZDBSKT' }
   },
-  {
-    title: 'Planet Zoo',
-    slug: 'planet-zoo-console-edition',
-    productId: '9nz038kz68b7',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Planet Coaster',
-    slug: 'planet-coaster-console-edition',
-    productId: '9ngrx7l5nb6m',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Planet Coaster 2',
-    slug: 'planet-coaster-2',
-    productId: '9PK5WS0HXQKQ',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'House Flipper',
-    slug: 'house-flipper',
-    productId: '9nbfgkqlmv33'
-  },
-  {
-    title: 'House Flipper 2',
-    slug: 'house-flipper-2',
-    productId: '9n67z2d5phw2'
-  },
-  {
-    title: 'PowerWash Simulator',
-    slug: 'powerwash-simulator',
-    productId: '9nhdjc0nw20m'
-  },
-  {
-    title: 'Two Point Hospital',
-    slug: 'two-point-hospital-jumbo-edition',
-    productId: '9PFK1TLLZGXD'
-  },
-  {
-    title: 'Two Point Campus',
-    slug: 'two-point-campus',
-    productId: '9pcw1smn9rgg'
-  },
-  {
-    title: 'Two Point Museum',
-    slug: 'two-point-museum',
-    productId: '9nfhrf1m4w3z'
-  },
-  {
-    title: 'Frog Detective',
-    slug: 'frog-detective-the-entire-mystery',
-    productId: '9n0m3gvb0knn'
-  },
-  {
-    title: 'Venba',
-    slug: 'venba',
-    productId: '9p5p55s7hb9s'
-  },
-  {
-    title: 'Untitled Goose Game',
-    slug: 'untitled-goose-game',
-    productId: '9mvtctdthh23'
-  },
-  {
-    title: 'Hogwarts Legacy',
-    slug: 'hogwarts-legacy-xbox-series-x-s-version',
-    productId: '9mt5nj5w7b8z',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Botany Manor',
-    slug: 'botany-manor',
-    productId: '9mzt90b954xf'
-  },
-  {
-    title: 'Monument Valley',
-    slug: 'monument-valley',
-    productId: '9nt17x2mr55h',
-    unavailable: ['th-th', 'en-ph']
-  },
-  {
-    title: 'Monument Valley 2',
-    slug: 'monument-valley-2',
-    productId: '9ntxjp9733wk',
-    unavailable: ['th-th', 'en-ph']
-  },
+  { title: 'Planet Zoo', slug: 'planet-zoo-console-edition', productId: '9nz038kz68b7', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Planet Coaster', slug: 'planet-coaster-console-edition', productId: '9ngrx7l5nb6m', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Planet Coaster 2', slug: 'planet-coaster-2', productId: '9PK5WS0HXQKQ', unavailable: ['th-th', 'en-ph'] },
+  { title: 'House Flipper', slug: 'house-flipper', productId: '9nbfgkqlmv33' },
+  { title: 'House Flipper 2', slug: 'house-flipper-2', productId: '9n67z2d5phw2' },
+  { title: 'PowerWash Simulator', slug: 'powerwash-simulator', productId: '9nhdjc0nw20m' },
+  { title: 'Two Point Hospital', slug: 'two-point-hospital-jumbo-edition', productId: '9PFK1TLLZGXD' },
+  { title: 'Two Point Campus', slug: 'two-point-campus', productId: '9pcw1smn9rgg' },
+  { title: 'Two Point Museum', slug: 'two-point-museum', productId: '9nfhrf1m4w3z' },
+  { title: 'Frog Detective', slug: 'frog-detective-the-entire-mystery', productId: '9n0m3gvb0knn' },
+  { title: 'Venba', slug: 'venba', productId: '9p5p55s7hb9s' },
+  { title: 'Untitled Goose Game', slug: 'untitled-goose-game', productId: '9mvtctdthh23' },
+  { title: 'Hogwarts Legacy', slug: 'hogwarts-legacy-xbox-series-x-s-version', productId: '9mt5nj5w7b8z', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Botany Manor', slug: 'botany-manor', productId: '9mzt90b954xf' },
+  { title: 'Monument Valley', slug: 'monument-valley', productId: '9nt17x2mr55h', unavailable: ['th-th', 'en-ph'] },
+  { title: 'Monument Valley 2', slug: 'monument-valley-2', productId: '9ntxjp9733wk', unavailable: ['th-th', 'en-ph'] },
   {
     title: 'ABZU',
-    slug: {
-      default: 'abzu',
-      'th-th': 'abzu',
-      'en-ph': 'abzu'
-    },
-    productId: {
-      default: 'bq2307m831zg',
-      'th-th': '9PNLF7RLZRVV',
-      'en-ph': '9PNLF7RLZRVV'
-    }
+    slug: { default: 'abzu', 'th-th': 'abzu', 'en-ph': 'abzu' },
+    productId: { default: 'bq2307m831zg', 'th-th': '9PNLF7RLZRVV', 'en-ph': '9PNLF7RLZRVV' }
   },
-  {
-    title: 'Spiritfarer',
-    slug: 'spiritfarer-farewell-edition',
-    productId: '9ng5l58fd3x5'
-  },
-  {
-    title: 'Sky: Children of the Light',
-    slug: 'sky-children-of-the-light',
-    productId: '9PB9GHRXD7GC'
-  },
-
-  {
-    title: 'Dave the Diver',
-    slug: 'dave-the-diver',
-    productId: '9PC1ZMD65L0T'
-  },
+  { title: 'Spiritfarer', slug: 'spiritfarer-farewell-edition', productId: '9ng5l58fd3x5' },
+  { title: 'Dave the Diver', slug: 'dave-the-diver', productId: '9PC1ZMD65L0T' },
 ];
 
-// Xbox.com uses locale codes, not ISO country codes
 const regions = ['en-us', 'en-gb', 'de-de', 'fr-fr', 'en-au', 'en-ca', 'pt-pt', 'es-es', 'pt-br', 'sv-se', 'en-in', 'nl-nl', 'pl-pl', 'th-th', 'da-dk', 'en-ph', 'cs-cz', 'hu-hu', 'en-za', 'es-mx', 'fi-fi'];
 
 (async () => {
@@ -379,7 +110,7 @@ const regions = ['en-us', 'en-gb', 'de-de', 'fr-fr', 'en-au', 'en-ca', 'pt-pt', 
 
     for (const region of regions) {
       if (game.unavailable && game.unavailable.includes(region)) {
-        console.warn(`🚫 Skipping ${game.title} in ${region} (marked unavailable)`);
+        console.warn(`Skipping ${game.title} in ${region} (marked unavailable)`);
         results[game.title][region] = 'unavailable';
         continue;
       }
@@ -401,24 +132,44 @@ const regions = ['en-us', 'en-gb', 'de-de', 'fr-fr', 'en-au', 'en-ca', 'pt-pt', 
         await new Promise(resolve => setTimeout(resolve, 5000));
 
         const priceData = await page.evaluate(() => {
+          // Matches: £11.99, $14.99, 14,99 €, 149,00 kr, 59,99 zł, 299,00 Kč, R149,00, R$57,99, 940,00 ₹
+          // Must have at least one digit before and after separator
+          function extractPrice(text) {
+            if (!text) return null;
+            // Currency before number: £11.99, $14.99, €14,99, ₹940, ₱299, R$57,99
+            let m = text.match(/[£$€₹₱]\s?\d[\d,.]*\d|\d[\d,.]*\d\s*€/);
+            if (m) return m[0].trim();
+            // R$ (Brazilian Real)
+            m = text.match(/R\$\s?\d[\d,.]*\d/);
+            if (m) return m[0].trim();
+            // R followed by digits (South Africa)
+            m = text.match(/\bR\d[\d,.]*\d/);
+            if (m) return m[0].trim();
+            // Number before currency: 14,99 €, 149,00 kr, 59,99 zł, 299,00 Kč
+            m = text.match(/\d[\d,.]*\d\s*(?:kr\.?|zł|Kč|Ft)/);
+            if (m) return m[0].trim();
+            return null;
+          }
+
           const buttons = Array.from(document.querySelectorAll('button[data-m]'));
 
-          // Find Buy button by its id in data-m — works in all languages
+          // Find Buy button by data-m id — language agnostic
           const buyButton = buttons.find(btn => {
-            try {
-              const dm = JSON.parse(btn.getAttribute('data-m') || '{}');
-              return dm.id === 'BuyButton';
-            } catch { return false; }
+            try { return JSON.parse(btn.getAttribute('data-m') || '{}').id === 'BuyButton'; }
+            catch { return false; }
           });
 
           if (buyButton) {
-            const ariaLabel = buyButton.getAttribute('aria-label') || '';
-            // Handles: £14.99, $14.99, 14,99 €, 14.99 €, 940,00 ₹, etc.
-            const priceMatch = ariaLabel.match(/[£$€₹₱R]\s?[\d,.]+|[\d,.]+\s*[£$€₹₱]|[\d,.]+\s*(kr\.?|zł|Kč|Ft|R\$)/);
-            if (priceMatch) {
-              const clean = priceMatch[0].trim().replace(/[,.]$/, '');
-              return { price: clean, originalPrice: null };
+            // Try price span inside button first — most reliable
+            const priceSpan = buyButton.querySelector('[class*="listedPrice"], [class*="boldText"]');
+            if (priceSpan) {
+              const spanPrice = extractPrice(priceSpan.innerText.trim());
+              if (spanPrice) return { price: spanPrice.replace(/[,.]$/, ''), originalPrice: null };
             }
+
+            // Fall back to aria-label
+            const ariaPrice = extractPrice(buyButton.getAttribute('aria-label') || '');
+            if (ariaPrice) return { price: ariaPrice.replace(/[,.]$/, ''), originalPrice: null };
           }
 
           // Free to play detection
@@ -440,7 +191,7 @@ const regions = ['en-us', 'en-gb', 'de-de', 'fr-fr', 'en-au', 'en-ca', 'pt-pt', 
         results[game.title][region] = priceData;
 
         if (!priceData.price) {
-          console.warn(`⚠️ No price found for ${game.title} in ${region}`);
+          console.warn(`No price found for ${game.title} in ${region}`);
         } else {
           console.log(`✅ ${game.title} (${region}): ${priceData.price}`);
         }
